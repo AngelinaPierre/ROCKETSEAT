4fb7552877aff42995ac9fc2a0c48ef092e4e2ba
AngelinaPierre <gabrielle.tripodi@hotmail.com>
2021-03-03 09:13:36 -0300

    FIM variaveis

---
+++ GUIA JAVASCRIPT/VARIÁVEIS/DECLARATION_ASSIGNMENT_VAR.js
@@ -0,0 +1,29 @@
+
+// Variáveis e tipos dados
+var name  // declaração e inicialização.
+
+// assignment or atribuição de valores
+name = "Mike"
+
+
+// what type of data is in the variable?
+console.log(typeof name)
+
+// let age = 20 // numver
+// let isHuman = true // boolean
+
+// we can put together the declarations
+
+let age, isHuman
+age = 18
+isHuman = true
+
+// multiplos argumentos na função
+// console.log(name, age, isHuman)
+
+//writing of text plus variables
+// Concatenando valores
+// console.log('O ' + name + ' tem ' + age +' anos ') //how to replace 'mike' and '18'?
+
+// interpolando valores com template literals or template strings
+console.log(`o ${name} tem ${age} anos`)
