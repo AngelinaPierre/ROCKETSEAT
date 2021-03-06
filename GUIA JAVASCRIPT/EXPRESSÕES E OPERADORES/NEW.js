b511eb7e97f1c925166f8df6a16ad58a8ab5c76f
AngelinaPierre <gabrielle.tripodi@hotmail.com>
2021-03-06 09:48:46 -0300

    expressões e operadores

---
+++ GUIA JAVASCRIPT/EXPRESSÕES E OPERADORES/NEW.js
@@ -0,0 +1,19 @@
+/*
+    NEW
+
+    * left-hand-side expression
+    * criar um novo objeto
+
+
+*/ 
+
+
+// let name = new String('Angelina')
+// name.surName = "Pierre"
+
+// let age = new Number(23)
+
+// console.log(name.surName,age)
+
+let data = new Date('2020-12-01')
+console.log(data.__proto__)
---
+++ GUIA JAVASCRIPT/EXPRESSÕES E OPERADORES/UNARY.js
@@ -0,0 +1,17 @@
+/*
+    OPERADORES UNÁRIOS
+    TYPEOF
+    DELETE
+
+
+*/ 
+
+const person = {
+    name: "angelina",
+    age: 25,
+}
+// console.log(person)
+
+delete person.age
+console.log(person)
+
